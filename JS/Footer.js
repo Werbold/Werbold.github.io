a2e487c49ba8b7b23d96ea1849aab5d2bf37899e
Werbold <64532263+Werbold@users.noreply.github.com>
2020-12-20 22:07:52 +0100

    0

--- JS/Footer.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function() {
-	$('footer').after("<div id='footerText'><div class='left'><p>Vytvořili Matouš Demko & Kristýna Tůmová</p></div><div class='center'><p>matous.demko@calc.it<br>kristyna.tumova@calc.it</p></div><div class='right'><p>Copyright<sup>©</sup> 2020</p></div></div>");
-});
