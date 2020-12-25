1a481d0fd7f14ea19524ba4401fc403166969ec0
Werbold <64532263+Werbold@users.noreply.github.com>
2020-12-25 16:49:43 +0100

    d

--- JS/Footer.js
+++ JS/Footer.js
@@ -1,3 +1,3 @@
 $(document).ready(function() {
-	$('footer').after("<div id='footerText'><div class='left'><p>Vytvořili Matouš Demko & Kristýna Tůmová</p></div><div class='center'><p>matous.demko@calcit.eu<br>kristyna.tumova@calcit.eu</p></div><div class='right'><p>Copyright<sup>©</sup> 2020</p></div></div>");
+	$('footer').after("<div id='footerText'><div class='left'><p>Matouš Demko & Kristýna Tůmová</p></div><div class='center'><p>support@calcit.eu</p></div><div class='right'><p>Copyright<sup>©</sup> 2020 calcit.eu</p></div></div>");
 });
