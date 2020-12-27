e4cdb97ecf1c867d350c5909a789e6d74ea3c18d
Werbold <64532263+Werbold@users.noreply.github.com>
2020-12-27 14:38:20 +0100

    Oprava chyb

--- JS/Footer.js
+++ JS/Footer.js
@@ -1,3 +1,3 @@
 $(document).ready(function() {
-	$('footer').after("<div id='footerText'><div class='left'><p>Matouš Demko & Kristýna Tůmová</p></div><div class='center'><p>support&#64;calcit.eu</p></div><div class='right'><p>Copyright<sup>©</sup> 2020 calcit.eu</p></div></div>");
+	$('footer').after("<div id='footerText'><div class='left'><p>Matouš Demko & Kristýna Tůmová</p></div><div class='center'><p>support&#64;calcit.eu</p></div><div class='right'><p>Copyright<sup>©</sup> 2021 calcit.eu</p></div></div>");
 });
