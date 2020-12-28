950622b741bd28c94b803a4955cd002102b13424
Werbold <64532263+Werbold@users.noreply.github.com>
2020-12-28 12:19:51 +0100

    Update PythagorovaVeta.js

--- CZ/Matematika/Trojuhelnik/PythagorovaVeta/JavaScript/PythagorovaVeta.js
+++ CZ/Matematika/Trojuhelnik/PythagorovaVeta/JavaScript/PythagorovaVeta.js
@@ -13,9 +13,9 @@ $(document).ready(function() {
 			if (cislo1 > 0 && cislo2) {
 				vysledekC = Math.sqrt(Math.pow(cislo1,2) + Math.pow(cislo2,2));
 
-				vysledekC = vysledekO * 1000;
-				vysledekC = Math.round(vysledekO);
-				vysledekC = vysledekO / 1000;
+				vysledekC = vysledekC * 1000;
+				vysledekC = Math.round(vysledekC);
+				vysledekC = vysledekC / 1000;
 
 				$('#Vysledek').after("<p class='count'>Strana a: " + cislo1 + " " + jednotka + "<br>" 
 				+ "Strana b: " + cislo2 + " " + jednotka + "<br>"
@@ -24,9 +24,9 @@ $(document).ready(function() {
 			else if (cislo1 > 0 && cislo3 > cislo1) {
 				vysledekB = Math.sqrt(Math.pow(cislo3,2) - Math.pow(cislo1,2));
 
-				vysledekB = vysledekO * 1000;
-				vysledekB = Math.round(vysledekO);
-				vysledekB = vysledekO / 1000;
+				vysledekB = vysledekB * 1000;
+				vysledekB = Math.round(vysledekB);
+				vysledekB = vysledekB / 1000;
 
 				$('#Vysledek').after("<p class='count'>Strana a: " + cislo1 + " " + jednotka + "<br>" 
 				+ "Strana c: " + cislo3 + " " + jednotka + "<br>"
@@ -35,9 +35,9 @@ $(document).ready(function() {
 			else if (cislo2 > 0 && cislo3 > cislo2) {
 				vysledekA = Math.sqrt(Math.pow(cislo3,2) - Math.pow(cislo2,2));
 
-				vysledekA = vysledekO * 1000;
-				vysledekA = Math.round(vysledekO);
-				vysledekA = vysledekO / 1000;
+				vysledekA = vysledekA * 1000;
+				vysledekA = Math.round(vysledekA);
+				vysledekA = vysledekA / 1000;
 
 				$('#Vysledek').after("<p class='count'>Strana b: " + cislo2 + " " + jednotka + "<br>" 
 				+ "Strana c: " + cislo3 + " " + jednotka + "<br>"
