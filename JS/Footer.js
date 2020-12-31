95e34d4b33e0e14988f56fa82e32df70826e412f
Werbold <64532263+Werbold@users.noreply.github.com>
2020-12-31 23:27:34 +0100

    sc

--- JS/Footer.js
+++ JS/Footer.js
@@ -1,3 +1,3 @@
 $(document).ready(function() {
-	$('footer').after("<div id='footerText'><div class='left'><p>Matouš Demko & Kristýna Tůmová</p></div><div class='center'><p>support&#64;calcit.eu</p></div><div class='right'><p>Copyright<sup>©</sup> 2021 calcit.eu</p></div></div>");
+	$('footer').after("<div id='footerText'><div class='left'><p id='names'>Matouš Demko <br> <span id='and'>&</span> <br>Kristýna Tůmová</p></div><div class='center'><p>support&#64;calcit.eu</p></div><div class='right'><p>Copyright © 2021 calcit.eu</p></div></div>");
 });
