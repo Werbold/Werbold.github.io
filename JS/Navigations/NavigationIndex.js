aa489ebf891ad81172976c00be128c44b10aca12
Werbold <64532263+Werbold@users.noreply.github.com>
2021-04-10 12:38:17 +0200

    Delete

--- JS/Navigations/NavigationIndex.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-		/*Navigace*/
-	$("#navigation").after("<div id='hlavicka'><div id='topHeader'><p><span id='headerText'><a href='#' title='Bug report' target='_blank'>Našli jste chybu? Dejte nám vědět.</a></span><span class='cara'>|</span> <span id='headerEmail'>support&#64;calcit.eu</span></p></div><!--Logo--><div id='logoContain'><p><img id='logo' src='Images/Kalcit/Kalcit.png' alt='Logo - Kalcit'><span id='Calcit'>Calcit </span><span id='definition'>- Databáze vzorečků a online kalkulátorů</span></p></div><!--Navigace--><div id='bottomHeader'><nav><ul><li><a href='index.html' title='Domů'>Domů</a></li><li><a href='#' title='O Calcitu'>O Calcitu</a></li><li><a href='CZ/Fyzika/Fyzika.html' title='Fyzika'>Fyzika</a></li><li><a href='CZ/Matematika/Matematika.html' title='Matematika'>Matematika</a></li></ul></nav></div></div>");
-});
--- JS/Navigations/NavigationZaostreno.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-		/*Navigace*/
-	$("#navigation").after("<div id='hlavicka'><div id='topHeader'><p><a href='#' title='Bug report' target='_blank'>Našli jste chybu? Dejte nám vědět.</a><span class='cara'>|</span> support&#64;calcit.eu</p></div><!--Logo--><div id='logoContain'><p><img id='logo' src='../../../../Images/Kalcit/Kalcit.png' alt='Logo - Kalcit'><span id='Calcit'>Calcit </span><span id='definition'>- Databáze vzorečků a online kalkulátorů</span></p></div><!--Navigace--><div id='bottomHeader'><nav><ul><li><a href='../../../../index.html' title='Domů'>Domů</a></li><li><a href='#' title='O Calcitu'>O Calcitu</a></li><li><a href='../../../Fyzika/Fyzika.html' title='Fyzika'>Fyzika</a></li><li><a href='../../../Matematika/Matematika.html' title='Matematika'>Matematika</a></li></ul></nav></div></div>");
-});
