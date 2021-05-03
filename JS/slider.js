64b980be8a0b799a053efa7736eb1fe3e2c7dccb
Werbold <64532263+Werbold@users.noreply.github.com>
2021-05-03 20:31:35 +0200

    +

---
+++ JS/slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+	$('.slider').on('click', function () {
+		if ($('.active').next('.slide').length) {
+			$('.active').removeClass('active').next('.slide').addClass('active');
+		} else {
+			$('.active').removeClass('active');
+			$('.slide').first().addClass('active');
+		}
+	});
+});
